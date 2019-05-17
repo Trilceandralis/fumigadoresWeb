dc19ad67918cd0322e9b5575d2f879e000595500
tomi <decampostomas@gmail.com>
2019-05-16 23:37:51 -0300

    primer commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+
+<header class="main-header">
+  <img src="" alt="" class="">
+  <nav class="nav1">
+    <ul class="izquierda">
+      <li><a href="home.php"> HOME </a></li>
+      <li><a href="preguntas.php"> PREGUNTAS </a></li>
+    </ul>
+  </nav>
+  <nav class="nav2">
+    <ul class="derecha">
+      <li><a href="login.php"> LOGIN </a></li>
+      <li><a href="register.php"> REGISTRO </a></li>
+    </ul>
+  </nav>
+</header>
+
+?>
